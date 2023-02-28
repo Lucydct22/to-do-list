1393406824153d7c1ada7a70879e6ef9f64c3163
lucydct <tenalucia0@gmail.com>
2023-02-28 10:12:11 +0100

    todo error message show up

--- src/todo.js
+++ src/todo.js
@@ -1,88 +1,3 @@
-// import React, { useState } from "react";
-
-// const Todo = () => {
-//   const [todos, setTodos] = useState([]);
-//   const [newTodo, setNewTodo] = useState("");
-
-//   const handleAddTodo = () => {
-//     setTodos([...todos, newTodo]);
-//     setNewTodo("");
-//   };
-
-//   const handleDeleteTodo = (index) => {
-//     const newTodos = [...todos];
-//     newTodos.splice(index, 1);
-//     setTodos(newTodos);
-//   };
-
-//   return (
-//     <div>
-//       <h1>To-Do List</h1>
-//       <ul>
-//         {todos.map((todo, index) => (
-//           <li key={index}>
-//             {todo}
-//             <button onClick={() => handleDeleteTodo(index)}>Delete</button>
-//           </li>
-//         ))}
-//       </ul>
-//       <input
-//         type="text"
-//         value={newTodo}
-//         onChange={(e) => setNewTodo(e.target.value)}
-//       />
-//       <button onClick={handleAddTodo}>Add</button>
-//     </div>
-//   );
-// };
-
-// export default Todo;
-
-// import React, { useState } from "react";
-// import "./todo.css";
-
-// const Todo = () => {
-//   const [todos, setTodos] = useState([]);
-//   const [newTodo, setNewTodo] = useState("");
-
-//   const handleAddTodo = () => {
-//     setTodos([...todos, newTodo]);
-//     setNewTodo("");
-//   };
-
-//   const handleDeleteTodo = (index) => {
-//     const newTodos = [...todos];
-//     newTodos.splice(index, 1);
-//     setTodos(newTodos);
-//   };
-
-//   return (
-//     <div className="todo-container">
-//       <h1>To-Do List</h1>
-//       <ul className="todo-list">
-//         {todos.map((todo, index) => (
-//           <li key={index}>
-//             {todo}
-//             <button onClick={() => handleDeleteTodo(index)}>Delete</button>
-//           </li>
-//         ))}
-//       </ul>
-//       <input
-//         type="text"
-//         value={newTodo}
-//         onChange={(e) => setNewTodo(e.target.value)}
-//         className="todo-input"
-//         placeholder="Add a new todo item"
-//       />
-//       <button onClick={handleAddTodo}>Add</button>
-//     </div>
-//   );
-// };
-
-// export default Todo;
-
-// Note that in the Todo.js file, we've added className attributes to the div, ul, li, button, and input elements to apply the styles from the Todo.css file. We've also added a placeholder attribute to the input element to give users an idea of what to input.
-
 import React, { useState, useEffect } from "react";
 import "./todo.css";
 
@@ -91,6 +6,7 @@ const Todo = () => {
   const [newTodo, setNewTodo] = useState("");
   const [editTodo, setEditTodo] = useState(null);
   const [editedTodo, setEditedTodo] = useState("");
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const savedTodos = localStorage.getItem("todos");
@@ -104,6 +20,11 @@ const Todo = () => {
   }, [todos]);
 
   const handleAddTodo = () => {
+    if (newTodo === "") {
+      setError(true);
+      return;
+    }
+    setError(false);
     setTodos([...todos, newTodo]);
     setNewTodo("");
   };
@@ -114,27 +35,32 @@ const Todo = () => {
     setTodos(newTodos);
   };
 
-  const handleEditTodo = (index) => {
-    setEditTodo(index);
-    setEditedTodo(todos[index]);
-  };
+  // const handleEditTodo = (index) => {
+  //   setEditTodo(index);
+  //   setEditedTodo(todos[index]);
+  // };
 
-  const handleUpdateTodo = (index) => {
-    const newTodos = [...todos];
-    newTodos[index] = editedTodo;
-    setTodos(newTodos);
-    setEditTodo(null);
-    setEditedTodo("");
-  };
+  // const handleUpdateTodo = (index) => {
+  //   const newTodos = [...todos];
+  //   newTodos[index] = editedTodo;
+  //   setTodos(newTodos);
+  //   setEditTodo(null);
+  //   setEditedTodo("");
+  // };
 
-  const handleCancelEdit = () => {
-    setEditTodo(null);
-    setEditedTodo("");
-  };
+  // const handleCancelEdit = () => {
+  //   setEditTodo(null);
+  //   setEditedTodo("");
+  // };
 
   return (
     <div className="todo-container">
       <h1>TO-DO LIST</h1>
+      {todos.length === 0 && (
+        <p className="error-message center-text">
+          No to-do items. Add a new item below!
+        </p>
+      )}
       <ul className="todo-list">
         {todos.map((todo, index) => (
           <li key={index}>
@@ -145,13 +71,13 @@ const Todo = () => {
                   value={editedTodo}
                   onChange={(e) => setEditedTodo(e.target.value)}
                 />
-                <button onClick={() => handleUpdateTodo(index)}>UPDATE</button>
-                <button onClick={handleCancelEdit}>CANCEL</button>
+                {/* <button onClick={() => handleUpdateTodo(index)}>UPDATE</button>
+                <button onClick={handleCancelEdit}>CANCEL</button> */}
               </>
             ) : (
               <>
                 {todo}
-                <button onClick={() => handleEditTodo(index)}>EDIT</button>
+                {/* <button onClick={() => handleEditTodo(index)}>EDIT</button> */}
                 <button onClick={() => handleDeleteTodo(index)}>REMOVE</button>
               </>
             )}
@@ -163,11 +89,10 @@ const Todo = () => {
         value={newTodo}
         onChange={(e) => setNewTodo(e.target.value)}
         className="todo-input"
-        placeholder="Please fill the gap to complete de to do items "
+        placeholder="Please place below the to do list"
       />
       <button onClick={handleAddTodo}>ADD NEW</button>
     </div>
   );
 };
-
 export default Todo;
